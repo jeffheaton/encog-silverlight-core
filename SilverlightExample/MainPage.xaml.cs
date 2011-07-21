7c34244c9dfcea068702b955447487def1fa7322
Jeff Heaton <jeff@jeffheaton.com>
2011-07-21 18:11:55 -0500

    Adding old 2.5.x version of Encog silverlight.  Will be upgraded to 3.0.

---
+++ SilverlightExample/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Encog;
+using Encog.Util.Banchmark;
+using System.Threading;
+using Encog.Engine;
+
+namespace SilverlightExample
+{
+    public partial class MainPage : UserControl, IStatusReportable
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void BackgroundThread()
+        {
+            EncogBenchmark benchmark = new EncogBenchmark(this);
+            string result = benchmark.Process();
+            Dispatcher.BeginInvoke(() => this.List.Items.Add("Final benchmark score(lower is better): " + result));
+        }
+
+        private void Hello_Click(object sender, RoutedEventArgs e)
+        {
+            Dispatcher.BeginInvoke(() => { this.Control.IsEnabled = false; });
+            Thread thread = new Thread(new ThreadStart(BackgroundThread));
+            thread.Start();
+
+        }
+
+        public void Report(int total, int current, string message)
+        {
+            Dispatcher.BeginInvoke(() => this.List.Items.Add(current + "/" + total + ":" + message));
+        }
+    }
+}
---
+++ encog-core-silverlight/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Encog for Silverlight 2.5")]
+[assembly: AssemblyDescription("Encog for Silverlight 2.5")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Heaton Research, Inc")]
+[assembly: AssemblyProduct("encog_core_silverlight")]
+[assembly: AssemblyCopyright("Copyright © Heaton Research 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a30bf50a-373c-4d89-8537-b81fdbd289b6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("2.5.0")]
+[assembly: AssemblyFileVersion("2.5.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
